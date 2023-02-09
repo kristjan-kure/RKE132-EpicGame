c5ce31fde8ef8285df1938a15a8a84a6320c0c52
kristjan-kure <kristjan.kure.1@gmail.com>
2023-02-09 02:02:23 +0200

    add week8 code

---
+++ Practice/Program.cs
@@ -0,0 +1,19 @@
+﻿string path = @"C:\Users\krist\source\repos\Week8EpicGame\Data";
+string fileName = "chuck.txt";
+
+
+string[] lines = File.ReadAllLines(Path.Combine(path, fileName));
+
+ShowFileContent(lines);
+
+static void ShowFileContent(string[] fileContentArray)
+{
+    int i = 1;
+
+    foreach(string line in fileContentArray)
+    {
+        Console.WriteLine($"{i}. {line}");
+        i++;
+    }
+}
+
